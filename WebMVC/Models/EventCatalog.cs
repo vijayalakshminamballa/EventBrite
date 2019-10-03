6f66980d4c52c4bb8320d14a017c9fe3c5afd81f
Vijaya-Bitta <53644890+Vijaya-Bitta@users.noreply.github.com>
2019-10-02 18:13:13 -0700

    Added Models and ViewModels

--- WebMVC/Models/EventCatalog.cs
+++ WebMVC/Models/EventCatalog.cs
@@ -7,5 +7,20 @@ namespace WebMVC.Models
 {
     public class EventCatalog
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+        public string Organizer { get; set; }
+        public string PictureUrl { get; set; }
+        public string AddressLine { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int EventCapacity { get; set; }
+        public int EventTypeId { get; set; }
+        public string  EventType { get; set; }
+        public int EventCategoryId { get; set; }
+        public string EventCategory { get; set; }
     }
 }
