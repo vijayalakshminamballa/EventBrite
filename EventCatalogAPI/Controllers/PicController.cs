a4c2be7f4d6ac22d577e8ff03abe4ea58c6c5f63
Your name <wjung6799@outlook.com>
2019-10-05 06:18:02 -0700

    upload photo function added

--- EventCatalogAPI/Controllers/PicController.cs
+++ EventCatalogAPI/Controllers/PicController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using IOFile = System.IO.File;
 
@@ -16,6 +17,7 @@ namespace EventCatalogAPI.Controllers
     public class PicController : ControllerBase
     {
        private readonly IHostingEnvironment _env;
+
         public PicController(IHostingEnvironment env)
         {
             _env = env;
@@ -28,6 +30,22 @@ namespace EventCatalogAPI.Controllers
             var path = Path.Combine($"{webRoot}/Pics/", $"pic{id}.jpg");
             var buffer = IOFile.ReadAllBytes(path);
             return File(buffer, "image/jpeg");
+        }
+
+        // POST: api/Image
+        [HttpPost]
+        public async Task Post(IFormFile file)
+        {
+            int id = 1;
+            var webRoot = _env.WebRootPath;
+            var uploads = Path.Combine($"{webRoot}/Pics/", $"pic{id}.jpg");
+            if (file.Length > 0)
+            {
+                using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
         }
     }
 }
